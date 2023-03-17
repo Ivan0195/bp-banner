66b70de67b22b527e95a47e6e57159cc365a4377
Ivan0195 <iihnatsenka95@gmail.com>
2023-03-17 11:31:03 +0300

    layout done

---
+++ .history/src/index_20230317092027.js
@@ -0,0 +1,15 @@
+import './index.html';
+import './index.scss';
+
+const month = document.getElementById('month');
+const year = document.getElementById('year');
+
+const onElementSelect = (element, target, link) => {
+    element.style.scale = 0.9;
+    element.style.opacity = 0.5;
+    target.style.scale = 1;
+    target.style.opacity = 1;
+}
+
+month.onclick = () => onElementSelect(year, month, '');
+year.onclick = () => onElementSelect(month, year);
---
+++ .history/src/index_20230317105920.js
@@ -0,0 +1,18 @@
+import './index.html';
+import './index.scss';
+
+const month = document.getElementById('month');
+const year = document.getElementById('year');
+const button = document.getElementById('button');
+let linkAdress;
+
+const onElementSelect = (element, target, link) => {
+    element.style.scale = 0.9;
+    element.style.opacity = 0.5;
+    target.style.scale = 1;
+    target.style.opacity = 1;
+    button.onclick = 'location.href=`'
+}
+
+month.onclick = () => onElementSelect(year, month, 'https://apple.com/');
+year.onclick = () => onElementSelect(month, year, 'https://google.com/ ');
